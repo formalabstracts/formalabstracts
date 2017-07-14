b90c584b34f9dc212b564362fe03cce884b4ec08
Rob Lewis <rob.y.lewis@gmail.com>
2017-07-14 15:44:48 -0400

    style(*): minor notational changes

--- fabstract/Bauer_A_InjBaireNat/fabstract.lean
+++ fabstract/Bauer_A_InjBaireNat/fabstract.lean
@@ -3,8 +3,10 @@ import folklore.categories
 import folklore.toposes
 import .realizability
 
-namespace Bauer_A_InjBaireNat
 noncomputable theory
+
+namespace Bauer_A_InjBaireNat
+
 
 -- we construct a partial combinatory algebra based on
 -- infinite-time Turing machines
@@ -20,14 +22,14 @@ constant Baire_to_N : T.underlying_category.hom Baire N
 axiom Baire_to_N_is_mono : monomorphism Baire_to_N
 
 open result
+
 def fabstract : meta_data := {
   description := "We construct a realizability topos in which the reals are embedded in the natural numbers. The topos is based on infinite-time Turing machines of Joel Hamkins.",
   authors := ["Andrej Bauer"],
   doi := ["https://doi.org/10.1017/S0960129513000406"],
-
   results := [Construction IITM,
               Construction Baire_to_N,
-              Proof Baire_to_N_is_mono],
+              Proof Baire_to_N_is_mono]
 }
 
 end Bauer_A_InjBaireNat
--- folklore/toposes.lean
+++ folklore/toposes.lean
@@ -3,14 +3,12 @@
 import .categories
 
 structure natural_numbers (C : category) :=
-  mk_natural_number_object ::
     (nno_object : C.obj)
     (nno_structure : Type)
 
 -- an incomplete definition of toposes
 -- this should probably be done using type classes
 structure topos :=
-  mk_topos ::
     (underlying_category : category)
     (nno : natural_numbers underlying_category)
     (exponent : Π (A B : underlying_category.obj), exponential A B)
