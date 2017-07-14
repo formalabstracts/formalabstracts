cc9f626b3411db2d80e6669f73953e7030bc36d2
Andrej Bauer <Andrej.Bauer@andrej.com>
2017-07-14 23:50:13 +0100

    A more detailed explanation of the `result` data type.

--- meta_data.lean
+++ meta_data.lean
@@ -1,4 +1,39 @@
--- The type of results
+/- Each fabstract contains a list of results. The following type lists the
+   various kinds of results. You may read the values as follows:
+
+   * `Proof p` -- the paper contains the proof `p`
+   * `Construction c` -- the paper contains the construction `c`
+   * `Conjecture C` -- the paper contains the conjecture `C`
+
+   Let us take a typical situation: you want to state that the paper
+   contains a proof of some statement `S`, but you do not want to actually
+   formalize the proof from the paper. In this case the fabstract would
+   contain
+
+       definition S := ⟨formalization of statement S⟩
+       axiom proof_of_S : S
+       ⋮
+       definition fabstract : meta_data := {
+          …,
+          results = […, Proof proof_of_S, …]
+       }
+
+   This way, by using `#print axioms fabstract`, you can find out precisely
+   which bits of the paper have not been formalized in the fabstract. You
+   can see what `proof_of_S` actually proves by inspecting its type.
+
+   Let us take another typical situation: the paper contains a construction
+   of an object `c` whose type is `T`, which is easy to formalize in Lean.
+   Then you would do
+
+       definition T := ⟨formalization of type T⟩
+       definition c := ⟨formalization of construction c⟩
+       ⋮
+       definition fabstract : meta_data := {
+          …,
+          results = […, Construction c, …]
+       }
+-/
 inductive {u} result : Type (u+1)
 | Proof : Π {P : Prop}, P → result
 | Construction : Π {A : Type u}, A → result
