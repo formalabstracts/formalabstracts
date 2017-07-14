dd9207b4a439ce89a9c4ce6b7f93cd43eb3de462
Andrej Bauer <Andrej.Bauer@andrej.com>
2017-07-14 18:17:58 +0100

    Fix statement of the Bauer_A paper, it was all wrong.

--- fabstract/Bauer_A_InjBaireNat/fabstract.lean
+++ fabstract/Bauer_A_InjBaireNat/fabstract.lean
@@ -10,26 +10,24 @@ noncomputable theory
 -- infinite-time Turing machines
 constant IITM : PCA
 
-noncomputable def T := RT IITM
+def T := RT IITM
 
-definition N := T.nno
-definition R := dedekind_reals T
+definition N := T.nno.nno_object
+definition Baire := (T.exponent N N).underlying_object
 
-constant R_to_N : T.underlying_category.hom N.nno_object R.reals_object
+constant Baire_to_N : T.underlying_category.hom Baire N
 
-axiom R_to_N_is_mono : monomorphism R_to_N
+axiom Baire_to_N_is_mono : monomorphism Baire_to_N
 
 open result
-meta def fabstract : meta_data := {
+def fabstract : meta_data := {
   description := "We construct a realizability topos in which the reals are embedded in the natural numbers. The topos is based on infinite-time Turing machines of Joel Hamkins.",
   authors := ["Andrej Bauer"],
   doi := ["https://doi.org/10.1017/S0960129513000406"],
 
   results := [Construction IITM,
-              Construction R_to_N,
-              Proof R_to_N_is_mono],
+              Construction Baire_to_N,
+              Proof Baire_to_N_is_mono],
 }
-
-#print axioms fabstract
 
 end Bauer_A_InjBaireNat
