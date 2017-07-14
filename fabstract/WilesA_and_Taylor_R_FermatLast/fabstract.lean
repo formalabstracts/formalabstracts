6c4660ad6f6d8bdea5a8550bfe4317a29a5482c4
Andrej Bauer <Andrej.Bauer@andrej.com>
2017-07-14 18:17:48 +0100

    Remove `meta`.

--- fabstract/WilesA_and_Taylor_R_FermatLast/fabstract.lean
+++ fabstract/WilesA_and_Taylor_R_FermatLast/fabstract.lean
@@ -6,13 +6,11 @@ namespace WilesA_and_Taylor_R_FermatLast
 axiom fermat_last_theorem :
   ∀ (x y z n : nat), x > 0 → y > 0 → n > 2 → x ^ n + y ^ n ≠ z ^ n
 
-meta definition fabstract : meta_data := {
+definition fabstract : meta_data := {
     authors := ["Andrew Wiles", "Richard Tylor"],
     doi := ["10.2307/2118559", "10.2307/2118560"],
     results := [result.Proof fermat_last_theorem],
     description := "A result in number theory conjectured by Pierre de Fermat and proved by Andrew Wiles and Richard Taylor. Coloquially referred to as Fermat Last Theorem."
     }
-
-#print axioms fabstract
 
 end WilesA_and_Taylor_R_FermatLast
