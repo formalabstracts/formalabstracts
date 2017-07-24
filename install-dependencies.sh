a0300ee0766b7c582d0cc4ed5088b5be7b61cd47
picrin <Adam Kurkiewicz>
2017-07-24 15:19:12 +0100

    cleaner solution for addons

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,7 +3,6 @@ mkdir lean
 compile=$?
 if [[ $compile == 0 ]]; then
     cd lean
-    sudo apt-get install git libgmp-dev cmake
     git clone https://github.com/leanprover/lean.git .
     mkdir -p build/release
     cd build/release
