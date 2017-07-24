6342f8f5dc6972c8d12b2eb179cc692266a2b9e2
picrin <Adam Kurkiewicz>
2017-07-24 15:55:54 +0100

    fix cmake options

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,6 +6,6 @@ if [[ $compile == 0 ]]; then
     git clone https://github.com/leanprover/lean.git .
     mkdir -p build/release
     cd build/release
-    cmake ../../src
-    make
+    cmake -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER ../../src
+    make -j2
 fi
