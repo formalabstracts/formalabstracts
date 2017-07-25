e23faeea6565bfd9afb3abc16e04294c1bc50b7f
picrin <Adam Kurkiewicz>
2017-07-25 12:22:34 +0100

    fix CI

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,11 +3,5 @@ set -xev
 if [[ ! -d /lean ]]; then
     mkdir /lean
     chown travis /lean
-    su travis
-    cd /lean
-    git clone --depth=50 https://github.com/leanprover/lean.git .
-    mkdir -p build/release
-    cd build/release
-    cmake -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER ../../src
-    make -j2
+    su -c "./download-lean.sh" -m travis
 fi
