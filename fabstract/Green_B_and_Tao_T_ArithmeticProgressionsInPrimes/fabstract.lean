69837958e5c1fb3aa1e694226e6ee3843b71b2bf
Scott Morrison <scott.morrison@anu.edu.au>
2017-07-29 21:32:20 +1000

    The Green-Tao theorem on arithmetic progressions in primes.

---
+++ fabstract/Green_B_and_Tao_T_ArithmeticProgressionsInPrimes/fabstract.lean
@@ -0,0 +1,22 @@
+import meta_data
+
+namespace Green_B_and_Tao_T_ArithmeticProgressionsInPrimes
+
+def prime (n : nat) : Prop := ∀ m < n, (m = 0) ∨ (m = 1) ∨ (n % m ≠ 0)
+
+-- A statement of Green & Tao's theorem about arithmetic progressions in primes
+axiom arithmetic_progressions_in_primes :
+∀ n k : nat, ∃ m r : nat, m ≥ n ∧ (∀ i < k, prime (m + i * r))
+
+-- They also prove various stronger statements; this is an important and easy to state consequence.
+
+definition fabstract : meta_data :=
+{ description := "The primes contain arbitrarily long arithmetic progressions",
+  authors := [
+    {name := "Ben Green"},
+    {name := "Terry Tao"}
+  ],
+  primary := cite.DOI "10.4007/annals.2008.167.481",
+  results := [result.Proof arithmetic_progressions_in_primes] }
+
+end Green_B_and_Tao_T_ArithmeticProgressionsInPrimes
