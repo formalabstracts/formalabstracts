e7c16462a0568e07f20072c1a9ba167cb610952f
Rob Lewis <rob.y.lewis@gmail.com>
2017-07-31 12:27:04 +0100

    change meta data definitions

--- meta_data.lean
+++ meta_data.lean
@@ -37,9 +37,29 @@
        }
 -/
 inductive {u} result : Type (u+1)
-  | Proof : Π {P : Prop}, P → result
-  | Construction : Π {A : Type u}, A → result
-  | Conjecture : Prop → result
+| Proof : Π {P : Prop}, P → result
+| Construction : Π {A : Type u}, A → result
+| Conjecture : Prop → result
+
+
+/- A datatype describing a person, normally an author. You may leave out
+   most fields, but name has to be there. -/
+structure author :=
+(name : string)
+(institution : string := "")
+(homepage : string := "")
+(email : string := "")
+
+structure document :=
+(title : string)
+(authors : list author)
+(doi : string := "") -- write evertying starting from the DOI prefix (which is 10)
+(arxiv : string := "") -- write these as Arxiv "1707.04448"
+(url : string := "")
+(source : string := "") -- journal or book title
+(year : option nat := none)
+(reference : string := "")
+
 
 /- There will be many citations everywhere, so it is a good idea
    to introduce a datatype for them early on. Here are some typical
@@ -76,20 +96,10 @@ inductive {u} result : Type (u+1)
 
 -/
 inductive cite
-  | DOI : string → cite -- write everything starting from the DOI prefix (which is 10)
-  | Arxiv : string → cite -- write these as Arxiv "1707.04448"
-  | URL : string → cite
-  | Reference : string → cite
-  | Ibidem : cite -- refer to the primary source of a fabstract
-  | Item : cite → string → cite -- refer to specific item in a source
-
-/- A datatype describing a person, normally an author. You may leave out
-   most fields, but name has to be there. -/
-structure author :=
-    (name : string)
-    (institution : string := "")
-    (homepage : string := "")
-    (email : string := "")
+| Document : document → cite
+| Website : string → cite
+| Folklore : cite
+| Item : cite → string → cite -- refer to specific item in a source
 
 /-
 TODO: This definition forces all the results in a particular fabstract
@@ -101,12 +111,13 @@ giving multiple equivalent sources (say a journal paper and the ArXiv version)
 makes it impossible to resolve conflicts when they arise. The primary source
 is always to be taken as the official one.
 -/
-structure {u} meta_data : Type (u+1) :=
-    (description : string) -- short description of the contents
-    (authors : list author) -- list of authors
-    (primary : cite) -- primary source (the most official one)
-    (secondary : list cite := []) -- auxiliary and alternative sources (such as ArXiv equivalent)
-    (results : list (result.{u})) -- the list of results
+structure meta_data :=
+(description : string) -- short description of the contents
+(contributors : list author := []) -- list of contributors
+(sources : list cite := [])
+
+structure {u} fabstract extends meta_data :=
+(results : list (result.{u}))
 
 /-
 Users will want to assume that a certain objects exist,
@@ -116,21 +127,21 @@ constants like this, we tag them with informal descriptions
 (of what the structure is, or how the construction goes)
 and references to the literature.
 -/
-structure unfinished_meta_data :=
-    (description : string)
-    (references : list cite := [])
+/-structure unfinished_meta_data :=
+(description : string)
+(references : list cite := [])-/
 
 section user_commands
 open lean.parser tactic interactive
 
 meta def add_unfinished (nm : name) (tp data : expr ff) : command :=
 do eltp ← to_expr tp,
-   eldt ← to_expr ``(%%data : unfinished_meta_data),
+   eldt ← to_expr ``(%%data : meta_data),
    let axm := declaration.ax nm [] eltp,
    add_decl axm,
    let meta_data_name := nm.append `_meta_data,
    add_decl $ mk_definition meta_data_name []
-                  `(unfinished_meta_data) eldt
+                  `(meta_data) eldt
 
 @[user_command]
 meta def unfinished_cmd (meta_info : decl_meta_info) (_ : parse $ tk "unfinished") : lean.parser unit :=
