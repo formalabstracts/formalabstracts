ad291de34702d8dea60153534f25cf4f431e35b0
Scott Morrison <scott@tqft.net>
2017-08-05 21:46:39 +1000

    renaming Lemma to Theorem

--- fabstract/Szemerédi_Theorem/fabstract.lean
+++ fabstract/Szemerédi_Theorem/fabstract.lean
@@ -2,7 +2,7 @@ import meta_data
 
 open function
 
-namespace Szemeredi_Lemma
+namespace Szemeredi_Theorem
 
 private def subset_of_0_until_N_with_at_least_m_elements ( N m : ℕ ) ( P : fin N → Prop ) : Prop :=
 ∃ f : fin m → { x // P x }, injective f
@@ -13,18 +13,18 @@ private def contains_arithmetic_progression_of_length' ( P : ℕ → Prop ) ( k 
 private def contains_arithmetic_progression_of_length { N : ℕ } ( P : fin N → Prop ) ( k : ℕ ) : Prop :=
 contains_arithmetic_progression_of_length' (λ n, dite (n < N) (λ w, P ⟨ n, w ⟩ ) (λ w, false) ) k
 
-axiom Szemeredi_Lemma :
+axiom Szemeredi_Theorem :
 ∀ k n : ℕ, 
   ∃ N : ℕ, 
     ∀ P : fin N → Prop, 
       subset_of_0_until_N_with_at_least_m_elements N (N/n) P → contains_arithmetic_progression_of_length P k
 
 definition fabstract : meta_data :=
-{ description := "A finitary statement of Szemerédi's lemma (that a subset of the natural numbers with positive upper density contains an arithmetic progression of any length).",
+{ description := "A finitary statement of Szemerédi's theorem, that a subset of the natural numbers with positive upper density contains an arithmetic progression of any length.",
   authors := [
     {name := "Endre Szemerédi"}
   ],
   primary := cite.URL "https://zbmath.org/?q=an:0303.10056",
   results := [result.Proof Szemeredi_Lemma] }
 
-end Szemeredi_Lemma
+end Szemeredi_Theorem
