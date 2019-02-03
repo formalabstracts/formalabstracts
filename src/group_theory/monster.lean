2d1939e1c3fdc07f6771bca84efe214e9fd3561f
Jesse Michael Han <jmh288@pitt.edu>
2019-02-03 16:35:00 -0500

    Move Coxeter Y-diagrams into presentation.lean

--- src/group_theory/monster.lean
+++ src/group_theory/monster.lean
@@ -11,33 +11,10 @@ There's a 12-generator 80-relator presentation for the Monster group. Specifical
 , one for each node in the Coxeter-Dynkin diagram;
 11 relators of the form (xy)3=1, one for each pair of adjacent nodes; 55 relators of the form (xy)2=1 (commutators), one for each pair of non-adjacent nodes; together with a single 'spider' relator, (ab₁c₁ab₂c₂ab₃c₃)^10=1 , which results in the group M×C2. We can get rid of the C2 by quotienting out by an eightieth relation, x=1, where x is the unique non-identity element in the centre of the group. -/ 
 
-@[derive decidable_eq] inductive coxeter_vertices {n} (xs : dvector ℕ n) : Type
-| torso : coxeter_vertices
-| arm : ∀ x : dfin n, dfin (xs.nth'' x + 1) → coxeter_vertices
-
-open coxeter_vertices
-
-inductive coxeter_edges_directed {n} (xs : dvector ℕ n) : 
-  coxeter_vertices xs → coxeter_vertices xs → Prop 
-| edge_torso : ∀ i : dfin n , coxeter_edges_directed (arm i dfin.fz) (torso _)
-| edge_arm : ∀ i : dfin n, ∀ v : dfin (xs.nth'' i + 1), v ≠ dfin.last _ → 
-    coxeter_edges_directed (arm i v) (arm i (v+1)) 
-
-inductive symmetric_closure {α : Type*} (E : α → α → Prop) : α → α → Prop
-| incl : ∀ a b : α, E a b → symmetric_closure a b
-| symm : ∀ a b : α, E a b → symmetric_closure b a
-
-def coxeter_edges {n} (xs : dvector ℕ n) : coxeter_vertices xs → coxeter_vertices xs → Prop :=
-symmetric_closure (coxeter_edges_directed xs)
-
--- TODO derive decidability instance
-noncomputable instance decidable_coxeter_edges {n} (xs : dvector ℕ n) : 
-  decidable_rel $ coxeter_edges xs :=
-λ _ _, classical.prop_decidable _
-
 noncomputable def Y443 : Group := coxeter_group $ matrix_of_graph (coxeter_edges [3,3,2])
 
 namespace monster
+open coxeter_vertices
 
 /- coxeter_vertices [p,q,r] consists of a left arm of length p+1, a right arm of length q+1, and
 a bottom arm of length r+1, with one node in the center connecting them.
