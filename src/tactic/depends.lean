e9068a4866c01e452f89ee864829ea556c478449
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2019-02-12 22:36:11 -0500

    A more comprehensive depends tactic along with functions for capturing metadata.

--- src/tactic/depends.lean
+++ src/tactic/depends.lean
@@ -9,8 +9,10 @@ A meta def called `#depends` which gives the names of all the theorems (the stat
 import data.buffer.parser
 import group_theory.mathieu_group
 import group_theory.euclidean_lattice
+import group_theory.sporadic_group 
+import measure_theory.giry_monad 
 
-open tactic expr interactive native name list lean.parser
+open tactic expr interactive nat native name list lean.parser environment
 
 /--Takes an expr and spits out a list of all the names in that expr -/
 meta def list_names (e : expr): list name :=
@@ -24,7 +26,7 @@ env.fold [] $ (λ d ns, d.to_name :: ns)
 meta def list_all_decls' (env : environment) : rb_set name :=
 env.fold (mk_rb_set) $ (λ d ns, ns.insert d.to_name)
 
-/-- Traces all declarations with prefix namesp in the current environment. -/
+/-- Traces all declarations with prefix `namesp` in the current environment. -/
 /-TODO : optimize using rb_set filters and maps(?)-/
 meta def trace_all_decls (namesp : name) : tactic unit :=
 do e ← get_env,
@@ -44,57 +46,58 @@ do e ← get_env,
     tactic.trace $ list_names d.type 
 
 
-meta def direct_dependencies : tactic unit :=
-do  t ← tactic.target,
-    let k := list_names t,
-    -- pure $ to_string k
-    tactic.trace k
+meta def name_dir_deps (n : name) : tactic(list name) := 
+do env ← get_env,
+    l ← get_decl n,
+    if is_structure env n then  
+    do fl ← returnopt $ structure_fields env n,
+        k ← mmap (λ h, do l ← get_decl h, pure $ list_names l.value) fl,
+    pure $ list.join k 
+    else 
+    pure $ list_names l.type
 
-theorem foo : 2+2 = 4 :=
+#check mmap
+run_cmd name_dir_deps `mathieu_group.steiner_system >>= tactic.trace 
+
+meta def name_dir_deps_depth (n : name) : ℕ → tactic(list name)
+| 0 := name_dir_deps n 
+| (succ m) :=
+ do l ← name_dir_deps_depth m <|> name_dir_deps n, 
+    l' ← mmap (λ h, name_dir_deps h) l, 
+    let k := list.erase_dup $ 
+    list.join (l :: l'),
+    tactic.trace k.length, 
+    pure $ k
+
+run_cmd do l ← mmap (λ h, name_dir_deps h) [`mathieu_group.steiner_system], tactic.trace l 
+
+theorem foo' : 2+2 = 4 :=
 begin
--- direct_dependencies,
--- trace_all_decls `name,
   simp,
 end
+ 
 
 namespace fabstract 
-meta structure fabstract (n : name):=
-(formal : expr)
-(informal : string)
-(depends : list name)
-
-
-meta def get_fabstract (n : name) : tactic (fabstract n) :=
-do 
-    resolved ← resolve_constant n, 
-    informal ← doc_string n, 
-    d ← get_decl resolved <|> fail ("could not retrieve given declration"),
-    let formal := d.type,
-    let depends := list_names d.type,
-    pure $ fabstract.mk n formal informal depends
 
 /- TODO : Declare a has_to_tactic_format instance for fabstract n -/
-meta def trace_fabstract (n : name) : tactic unit :=
-do f ← get_fabstract n,
-    tactic.trace "Formal statement: ",
-    tactic.trace f.formal,
-    tactic.trace " ",
-    tactic.trace "Informal statement: ",
-    tactic.trace f.informal,
-    tactic.trace " ",
-    tactic.trace "Dependencies: ",
-    tactic.trace f.depends,
-    tactic.trace " "
 
+
+meta def show_pos (n : name) : command :=
+do env   ← get_env,
+   pos   ← returnopt (env^.decl_pos n),
+   olean ← returnopt (env.decl_olean n) <|> return "current file",
+   trace $ to_string n ++ " was defined at " ++ olean ++ " : " ++ to_string pos.1 ++ ":" ++ to_string pos.2
 
 /- Tests -/
-#depends foo 
+#depends nat.has_one
 #depends group.equiv
-#depends isomorphic
-run_cmd trace_all_decls `mathieu_group
-run_cmd trace_fabstract `mathieu_group.Aut
-run_cmd trace_fabstract `euclidean_space_canonical_inclusion
-run_cmd trace_fabstract `determinant_spec 
-run_cmd trace_fabstract `leech_lattice_spec 
+#depends J4
+#depends nat.add._main
+#depends mclaughlin.McL
+#depends sends_identity_to_1
+
+set_option profiler true 
+run_cmd (name_dir_deps_depth `Suz 10) >>= tactic.trace
 
 end fabstract
+
---
+++ src/tactic/linenumber.lean
@@ -0,0 +1,75 @@
+/-
+Copyright (c) 2018 Koundinya Vajjha. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Koundinya Vajjha
+
+Generating meta data for a formal abstract. 
+-/
+
+import data.buffer.parser
+import group_theory.mathieu_group
+import group_theory.euclidean_lattice
+import group_theory.sporadic_group 
+-- import group_theory.lie_type
+
+open tactic expr interactive nat native name list lean.parser pexpr
+
+
+/--Takes an expr and spits out a list of all the names in that expr -/
+meta def list_names (e : expr): list name :=
+e.fold [] (λ e _ es, if is_constant e then insert e.const_name es else es)
+
+meta structure meta_data (n : name):=
+(type : expr)
+(value : expr)
+(informal : string)
+(depends : list name)
+(position : string)
+
+
+meta def show_pos (n : name) : tactic string :=
+do env   ← get_env,
+   pos   ← returnopt (env.decl_pos n),
+   olean ← returnopt (env.decl_olean n) <|> return "current file",
+   pure $ to_string n ++ " was defined at " ++ olean ++ " : " ++ to_string pos.1 ++ ":" ++ to_string pos.2
+
+/- -/
+meta def gen_metadata (n : name) : tactic (meta_data n) :=
+do 
+    resolved ← resolve_constant n, 
+    informal ← doc_string n,
+    d ← get_decl resolved <|> fail ("could not retrieve given declration"),
+    let type := d.type,
+    let value := d.value,
+    let depends := list_names d.type,
+    docstring ← doc_string n, 
+    position ← show_pos n,
+    pure $ meta_data.mk n type value informal depends position
+
+meta def trace_metadata (n : name) : tactic unit :=
+do f ← gen_metadata n,
+    tactic.trace "Type: ",
+    tactic.trace f.type,
+    tactic.trace " ",
+    tactic.trace "Value: ",
+    tactic.trace f.value,
+    tactic.trace " ",
+    tactic.trace "Informal statement: ",
+    tactic.trace f.informal,
+    tactic.trace " ",
+    tactic.trace "Dependencies: ",
+    tactic.trace f.depends,
+    tactic.trace " ",
+    tactic.trace "Position: ",
+    tactic.trace f.position,
+    tactic.trace " "
+
+/- Tests -/
+#check environment.is_structure
+run_cmd trace_metadata `mathieu_group.Aut
+run_cmd trace_metadata `euclidean_space_canonical_inclusion
+run_cmd trace_metadata `determinant_spec 
+
+#check list.empty
+-- run_cmd trace_metadata `dynkin_diagram
+
