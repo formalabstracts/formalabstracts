0b77d5b8d2415c82ddb4b471c0b2545e8521fb81
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2019-02-21 16:22:17 -0500

    declarations tagged with @[fabstract] now store their JSON metadata in their cache

--- src/tactic/fattribute.lean
+++ src/tactic/fattribute.lean
@@ -8,6 +8,7 @@ The fabstract user attribute.
 
 import tactic.basic 
 import tactic.ext 
+import tactic.linenumber
 
 open interactive interactive.types lean.parser tactic native
 
@@ -15,7 +16,7 @@ open interactive interactive.types lean.parser tactic native
 meta def fabstract_attr : user_attribute (rb_map name (list name)) (list name) :=
 {
   name := `fabstract,
-  descr := "The fabstract user attribute. Used to mark lemmas captured and also to store the 2010 MSC classification.",
+  descr := "The fabstract user attribute. Used to mark lemmas captured in order to capture meta data.",
   parser := many ident <|> pure [],
   cache_cfg := ⟨ λ l, l.mfoldl (λ m n, do
       tags ← fabstract_attr.get_param n,
@@ -23,30 +24,62 @@ meta def fabstract_attr : user_attribute (rb_map name (list name)) (list name) :
    , [] ⟩
 } 
 
+@[user_attribute]
+meta def fabstract_attr' : user_attribute (list string) (list name) :=
+{
+  name := `fabstract',
+  descr := "The fabstract user attribute. Used to mark lemmas captured in order to capture meta data.",
+  parser := many ident <|> pure [],
+  cache_cfg := ⟨ λ ns, mmap (trace_metadata_JSON) ns
+   , [] ⟩
+}
 /-- Well, hello there! -/
-@[fabstract ABC000 ABC200] 
+@[fabstract' ABC000 ABC200] 
 def test₁ : 1+1 =2 := by simp
 
-@[fabstract ABC101 ABC200] 
+@[fabstract' ABC101 ABC200] 
 def test₂ : 1+1 =2 := by simp
 
 @[fabstract ABC101 XYZ200] 
 def welp : 1+1 =2 := by simp
 
-@[fabstract JBX190 AXX200] 
+@[fabstract' JBX190 AXX200] 
 def woolp : 1+1 =2 := by simp
 
-@[fabstract] 
+@[fabstract'] 
 def flump : 1+1 =2 := by simp
 
 meta def get_MSC_codes (n : name) : tactic (list name) := user_attribute.get_param fabstract_attr n
 
 /- Tests -/
-run_cmd attribute.get_instances `fabstract >>= tactic.trace
-run_cmd doc_string `test₁ >>= tactic.trace >> get_MSC_codes `test₁ >>= tactic.trace 
+run_cmd attribute.get_instances `fabstract' >>= tactic.trace
+-- run_cmd doc_string `test₁ >>= tactic.trace >> get_MSC_codes `test₁ >>= tactic.trace 
 
 /- Gives all declarations with a particular MSC code.-/
 run_cmd do 
-  m ← fabstract_attr.get_cache,
-  v ← m.find `ABC101,
-  trace v
+  m ← fabstract_attr'.get_cache,
+  -- v ← m.find `ABC101,
+  trace m
+
+@[user_attribute]
+meta def foo_attr : user_attribute string :=
+{ name     := `foo, descr := "bar",
+  cache_cfg := {
+    mk_cache := λ ns, return $ string.join (list.map (λ n, to_string n ++ "\n") ns),
+    dependencies := []} }
+
+attribute [foo] eq.refl eq.mp
+
+set_option trace.user_attributes_cache true
+
+run_cmd do
+  s : string ← foo_attr.get_cache,
+  tactic.trace s,
+  s : string ← foo_attr.get_cache,
+  tactic.trace s,
+  foo_attr.set `eq.mpr () tt,
+  s : string ← foo_attr.get_cache,
+  tactic.trace s,
+  tactic.set_basic_attribute `reducible ``eq.mp, -- should not affect [foo] cache
+  s : string ← foo_attr.get_cache,
+tactic.trace s
