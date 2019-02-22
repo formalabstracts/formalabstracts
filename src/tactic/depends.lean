456d150d3dc4ec5ed242c55de95d345683ea332e
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2019-02-22 14:18:37 -0500

    better depends tactic

--- src/tactic/depends.lean
+++ src/tactic/depends.lean
@@ -7,27 +7,26 @@ A meta def called `#depends` which gives the names of all the theorems (the stat
 -/
 
 import data.buffer.parser
-import tactic.squeeze
+-- import group_theory.mathieu_group
+import group_theory.euclidean_lattice
+-- import group_theory.sporadic_group 
+-- import measure_theory.giry_monad 
 
-open tactic expr interactive native name list lean.parser parser
+open tactic expr interactive nat native name list lean.parser environment
 
-universe u
-
-variables {s : Type u} 
-
-/-Takes an expr and spits out a list of all the names in that expr -/
+/--Takes an expr and spits out a list of all the names in that expr -/
 meta def list_names (e : expr): list name :=
 e.fold [] (λ e _ es, if is_constant e then insert e.const_name es else es)
 
-/- Takes an environment and naively lists all declarations in it.-/
+/-- Takes an environment and naively lists all declarations in it.-/
 meta def list_all_decls (env : environment) : list name :=
 env.fold [] $ (λ d ns, d.to_name :: ns)
 
-/- Takes an environment and lists all declarations in it, much faster. -/
+/-- Takes an environment and lists all declarations in it, much faster. -/
 meta def list_all_decls' (env : environment) : rb_set name :=
 env.fold (mk_rb_set) $ (λ d ns, ns.insert d.to_name)
 
-/- Traces all declarations with prefix namesp in the current environment. -/
+/-- Traces all declarations with prefix `namesp` in the current environment. -/
 /-TODO : optimize using rb_set filters and maps(?)-/
 meta def trace_all_decls (namesp : name) : tactic unit :=
 do e ← get_env,
@@ -46,23 +45,77 @@ do e ← get_env,
     d ← get_decl resolved <|> fail ("declaration " ++ to_string given_name ++ " not found"),
     tactic.trace $ list_names d.type 
 
+/-- Return the direct dependencies of the *type* of a declaration. -/
+meta def name_dir_deps (n : name) : tactic(list name) := 
+do env ← get_env,
+    l ← get_decl n,
+    if is_structure env n then  
+    do fields ← returnopt $ structure_fields env n,
+        let res := map (λ h, name.append n h) fields,
+        k ← mmap (λ h, do l ← get_decl h, pure $ list_names l.type) res,
+        let clean := list.erase_dup (list.join k),
+        let final := list.filter (λ h, ¬ name.is_prefix_of n h) clean,
+        pure $ final 
+    else 
+    pure $ list_names l.type 
 
-meta def direct_dependencies : tactic unit :=
-do  t ← tactic.target,
-    let k := list_names t,
-    -- pure $ to_string k
-    tactic.trace k
+/-- Return the direct dependencies of the *value* of a declaration.-/
+meta def name_dir_deps_val (n : name) : tactic(list name) := 
+do env ← get_env,
+    l ← get_decl n,
+    if is_structure env n then  
+    do fields ← returnopt $ structure_fields env n,
+        let res := map (λ h, name.append n h) fields,
+        k ← mmap (λ h, do l ← get_decl h, pure $ list_names l.value) res,
+        let clean := list.erase_dup (list.join k),
+        let final := list.filter (λ h, ¬ name.is_prefix_of n h) clean,
+        pure $ final 
+    else 
+    pure $ list_names l.value 
 
-theorem foo : 2+2 = 4 :=
+-- run_cmd name_dir_deps `mathieu_group.steiner_system >>= tactic.trace 
+
+-- run_cmd name_dir_deps_val `J4 >>= tactic.trace  
+
+/-- Recursively return a joint list of the m-th sub-dependencies of the type of given name.-/
+meta def name_dir_deps_depth (n : name) : ℕ → tactic(list name)
+| 0 := name_dir_deps n 
+| (succ m) :=
+ do l ← name_dir_deps_depth m <|> name_dir_deps n, 
+    l' ← mmap (λ h, name_dir_deps h) l, 
+    let k := list.erase_dup $ 
+    list.join (l :: l'),
+    -- tactic.trace k.length, 
+    pure $ k
+
+-- run_cmd do l ← name_dir_deps_depth `mathieu_group.steiner_system 3, tactic.trace l 
+
+run_cmd name_dir_deps_depth `sends_identity_to_1 5 >>= tactic.trace
+
+/-- Recursively return a joint list of the m-th sub-dependencies of the type of given name.-/
+meta def name_dir_deps_depth_val (n : name) : ℕ → tactic(list name)
+| 0 := name_dir_deps_val n 
+| (succ m) :=
+ do l ← name_dir_deps_depth_val m <|> name_dir_deps_val n, 
+    l' ← mmap (λ h, name_dir_deps h) l, 
+    let k := list.erase_dup $ 
+    list.join (l :: l'),
+    -- tactic.trace k.length, 
+    pure $ k
+
+theorem foo' : 2+2 = 4 :=
 begin
--- direct_dependencies,
--- trace_all_decls `name,
   simp,
 end
--- #check eq_self_iff_true
--- #depends foo 
--- #depends cond_prob_swap
--- #depends total_prob
--- #depends group.equiv
--- #depends isomorphic
--- #depends is_simple_alternating_group
+
+
+/- Tests -/
+#depends nat.has_one
+#depends group.equiv
+-- #depends J4
+#depends nat.add._main
+-- #depends mclaughlin.McL
+#depends sends_identity_to_1
+    
+-- set_option profiler true 
+-- run_cmd (name_dir_deps_depth_val `mathieu_group.Aut 10) >>= tactic.trace
---
+++ src/tactic/fattribute.lean
@@ -0,0 +1,65 @@
+/-
+Copyright (c) 2018 Koundinya Vajjha. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Koundinya Vajjha
+
+The fabstract user attribute. 
+-/
+
+import tactic.basic 
+import tactic.metadata
+
+open interactive interactive.types lean.parser tactic native
+
+-- @[user_attribute]
+-- meta def fabstract_attr : user_attribute (rb_map name (list name)) (list name) :=
+-- {
+--   name := `fabstract,
+--   descr := "The fabstract user attribute. Used to mark lemmas captured in order to capture meta data.",
+--   parser := many ident <|> pure [],
+--   cache_cfg := ⟨ λ l, l.mfoldl (λ m n, do
+--       tags ← fabstract_attr.get_param n,
+--       return $ tags.foldl (λ m t, m.insert_cons t n) m) mk_rb_map
+--    , [] ⟩
+-- } 
+
+@[user_attribute]
+meta def fabstract_attr : user_attribute (rb_map name (string)) (list name) :=
+{
+  name := `fabstract,
+  descr := "The fabstract user attribute. Used to mark lemmas captured in order to capture meta data.",
+  parser := many ident <|> pure [],
+  cache_cfg := ⟨ 
+    λ ns, ( do l ← mmap (trace_metadata_JSON) ns, pure $ rb_map.of_list (list.zip ns l))
+   , [] ⟩
+}
+
+/- Tests -/
+/-- Well, hello there! -/
+@[fabstract ABC000 ABC200] 
+def test₁ : 1+1 =2 := by simp
+
+@[fabstract ABC101 ABC200] 
+def test₂ : 1+1 =2 := by simp
+
+@[fabstract ABC101 XYZ200] 
+def welp : 1+1 =2 := by simp
+
+@[fabstract JBX190 AXX200] 
+def woolp : 1+1 =2 := by simp
+
+@[fabstract] 
+def flump : 1+1 =2 := by simp
+
+meta def query_cache (n : name) : tactic (string) :=
+do m ← fabstract_attr.get_cache,
+  v ← m.find n,
+  pure v
+
+run_cmd attribute.get_instances `fabstract >>= tactic.trace
+
+run_cmd do 
+  m ← fabstract_attr.get_cache,
+  v ← m.find `flump,
+  trace m
+
